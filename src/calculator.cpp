ec24755f75f811bab108290ad8106dcfe597a276
Melvin Cabatuan <melvincabatuan@users.noreply.github.com>
2021-10-25 13:25:03 +0800

    Add files via upload

--- src/calculator.cpp
+++ src/calculator.cpp
@@ -1,26 +1,30 @@
+/*
+************* EDIT ME TO SOLVE THE LAB ACTIVITY ************* 
+*/
+
 #include "calculator.h"
 
 float add(float left, float right) {
-    return (left + right);
+    /* YOUR CODE HERE */
+    return 0.0f;
 }
 
 float subtract(float left, float right) {
-    return (left - right);
+    /* YOUR CODE HERE */
+    return 0.0f;
 }
 
 float multiply(float left, float right) {
-    return (left * right);
+    /* YOUR CODE HERE */
+    return 0.0f;
 }
 
 float divide(float left, float right) {
-    return (left / right);
+    /* YOUR CODE HERE */
+    return 0.0f;
 }
 
 int power(int base, int exponent) {
-    int result = 1;
-    for(int i = 1; i <= exponent; i++)
-    {
-        result *= base;
-    }
-    return result;
+    /* YOUR CODE HERE */
+    return 0;
 }
